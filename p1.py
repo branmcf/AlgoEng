9541823ef962f0df4a6bc47aaa58f154e9b4bc2e
Brandon McFarland <branmcf@gmail.com>
2019-04-05 18:09:06 -0500

    saving work

--- p1.py
+++ p1.py
@@ -3,6 +3,7 @@ import matplotlib
 import numpy as np
 import time
 matplotlib.use('TkAgg')
+from scipy.special import erfinv
 from collections import defaultdict
 from matplotlib import pyplot as plt
 from pdb import set_trace as bp
@@ -42,73 +43,76 @@ def generateAdjacency(sessions, distinctConflicts):
                 conflictAdjacency[session].append(key)
     return conflictAdjacency
 
-def formatConflicts(conflicts):
+def calculateDistinctConflicts2(conflicts):
+    distinctConflicts = set(frozenset(pair) for pair in conflicts)
+
+    return distinctConflicts
+
+def calculateDistinctConflicts1(conflicts):
+    
+    # create a dictionary for distinct conflicts
     distinctConflicts = {}
     for pair in conflicts:
-        element0 = pair[0]
-        element1 = pair[1]
+
+        # make the smallest value element 0
+        if pair[0] > pair[1]:
+            element0 = pair[0]
+            element1 = pair[1]
+        else:
+            element0 = pair[1]
+            element1 = pair[0]
+
+        # create a list of the keys that are in the dictionary
         conflictKeys = distinctConflicts.keys()
+
+        # if neither element is a key of the distinctConflicts dictionary
         if element0 not in conflictKeys and element1 not in conflictKeys:
+            # add key element0 to distinctConflicts and set the value to an empty list
             distinctConflicts[element0] = []
+            # assign a var to the new empty list
             targetList = distinctConflicts[element0]
+            # append the value of element1 to element0's list
             targetList.append(element1)
+
+        # if one of the elements is already a key in distinctConflicts append the value of the other
+        # element to the existing keys list
         elif element0 in conflictKeys and element1 not in distinctConflicts[element0]:
             targetList = distinctConflicts[element0]
             targetList.append(element1)
         elif element1 in conflictKeys and element0 not in distinctConflicts[element1]:
             targetList = distinctConflicts[element1]
             targetList.append(element0)
+
+        # Otherwise this is a dupicate, do nothing    
         elif element0 in conflictKeys and element1 in distinctConflicts[element0] or element1 in conflictKeys and element0 in distinctConflicts[element1]:
+            # duplicate, don't add
             continue
-    return distinctConflicts
 
-
-def calculateDistinctConflicts2(conflicts):
-    indexesWithDuplicates = []
-    conflictsNoDups = []
-    conflictTuples = [tuple(x) for x in conflicts]
-    def list_duplicates(seq):
-        tally = defaultdict(list)
-        for i,item in enumerate(seq):
-            tally[item].append(i)
-        return ((key,locs) for key,locs in tally.items() 
-                                if len(locs)>1)
-
-    for dup in sorted(list_duplicates(conflictTuples)):
-        dupIndexs = dup[1]
-        # push all dup indexes except for the first one
-        for x in range(1, len(dupIndexs)):
-            indexesWithDuplicates.append(dupIndexs[x])
-    
-    for idx, i in enumerate(conflicts):
-        if idx not in indexesWithDuplicates:
-            conflictsNoDups.append(i)
-    
-    distinctConflicts = formatConflicts(conflictsNoDups)
-
-    return distinctConflicts
-
-def calculateDistinctConflicts1(conflicts):
-    distinctConflicts = formatConflicts(conflicts)
     return distinctConflicts
 
 def calculateConflicts(schedule):
+    # create a list to hold the lists of pairwise conflicts
     conflictArray = []
     for key, value in schedule.iteritems():
+        # grab the session list of the attendee
         currentList = schedule[key]
+        # create lists of pairwise conflicts between attendee sessions
         for i in range(0, len(currentList) - 1):
             for x in range(i, len(currentList) - 1):
+                # add the pairwise conflict list to the conflict array
                 conflictArray.append([currentList[i], currentList[x+1]])
+
     return conflictArray
 
 def getRandomTiered(sessions):
+    # make a selection from the distribution
     num_sessions = sessions + 1
     first = np.random.randint(1, int(sessions * 0.1), int(sessions * 0.5))
     second = np.random.randint(int(sessions * 0.1) , sessions + 1, int(sessions * 0.5))
     final = np.append(first, second)
     selection = np.random.choice(final, 1)
     
-    # plotting
+    # code for plotting the distribution
     # data = np.random.choice(final, sessions)
     # print data
     # count, bins, ignored = plt.hist(final, int(sessions//10), facecolor='green') 
@@ -123,9 +127,10 @@ def getRandomTiered(sessions):
 
 
 def getRandomUniform(sessions):
+    # make a selection from the distribution
     selection = int(np.random.randint(1, sessions + 1, 1))
 
-    # plotting
+    # code for plotting the distribution
     # data = np.random.randint(1, 101, 2500)
     # count, bins, ignored = plt.hist(data, 50, facecolor='green') 
     # plt.xlabel('X~U[1,100]')
@@ -138,16 +143,17 @@ def getRandomUniform(sessions):
     return selection
 
 def getRandomSkewed(sessions):
+    # make a selection from the distribution
     a = int(np.random.randint(1, sessions + 1, 1))
     b = int(np.random.randint(1, sessions + 1, 1))
     c = abs(a-b)
     selection = c
 
-    # plotting
+    # code for plotting the distribution
     # a = np.random.randint(1, 101, 2500)
     # b = np.random.randint(1, 101, 2500)
     # c = abs(a-b)
-    # count, bins, ignored = plt.hist(c, 50, facecolor='green') 
+    # count, bins, ignored = plt.hist(c, 50, facecolor='green')  
     # plt.xlabel('X~U[1,100]')
     # plt.ylabel('Count')
     # plt.title("Skewed Distribution Histogram (Bin size 2)")
@@ -158,27 +164,43 @@ def getRandomSkewed(sessions):
     return selection
 
 def getCustom(sessions):
-    selection = int(np.random.randint(1, sessions + 1, 1))
+    # make a selection from the distribution
+    # selection = np.sqrt(int(np.random.randint(1, sessions + 1, 1))) * 10
 
-    # plotting
+    # a = int(np.random.randint(1, sessions + 1, 1))
+    # b = int(np.random.randint(1, sessions + 1, 1))
+
+    # code for plotting the distrib0ution
     # a = np.random.randint(1, 101, 2500)
-    # x = np.sqrt(a) * 10
-    # count, bins, ignored = plt.hist(x, 50, facecolor='green') 
-    # plt.xlabel('X~U[1,100]')
-    # plt.ylabel('Count')
-    # plt.title("Custom Distribution Histogram (Bin size 2)")
-    # plt.axis([1, 100, 0, 100])
-    # plt.grid(True)
-    # plt.show(block=True)
+    mean = 50
+    sigma = 15
+    a = np.random.uniform(size=10)
+    b = np.random.randint(1,11,10)
+
+    print a
+    print b
+    bp()
+    
+    x = mean + 2**0.5 * sigma * erfinv(int(np.random.uniform(size=500) * 2 - 1)
+    print x
+ 
+    count, bins, ignored = plt.hist(x, 50, facecolor='green') 
+    plt.xlabel('X~U[1,100]')
+    plt.ylabel('Count')
+    plt.title("Custom Distribution Histogram (Bin size 2)")
+    plt.axis([1, 100, 0, 100])
+    plt.grid(True)
+    plt.show(block=True)
 
     return selection
 
 def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDict):
     print 'Generating schedule...'
+    # uniform case
     if distribution == 'uniform':
         # I referenced the following url for uniform distribution code
         # https://stackoverflow.com/questions/22744577/plotting-basic-uniform-distribution-on-python
-        print 'Generating uniform distribution...'
+        print 'Sampling from uniform distribution...'
         # loop over the keys in the dict
         for key, value in attendeeDict.iteritems():
             # start with index 0
@@ -187,11 +209,11 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
             for s in attendeeDict[key]:
                 # if there is a default space in the array...
                 if None in attendeeDict[key]:
-                    # select a session
+                    # select a session using the uniform distribution
                     selection = getRandomUniform(sessions)
                     # while the selected session in already in the list...
                     while selection in attendeeDict[key]:
-                        # make a new selection.
+                        # make a new selection
                         selection = getRandomUniform(sessions)
                         # break when the selected session is not already in the list
                         if selection not in attendeeDict[key]:
@@ -205,9 +227,7 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
                     exit()
         return attendeeDict
     elif distribution == 'tiered':
-        print 'Generating tiered distribution...'
-        # TODO
-        print 'Generating tiered distribution...'
+        print 'Sampling from tiered distribution...'
         # loop over the keys in the dict
         for key, value in attendeeDict.iteritems():
             # start with index 0
@@ -216,7 +236,7 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
             for s in attendeeDict[key]:
                 # if there is a default space in the array...
                 if None in attendeeDict[key]:
-                    # select a session
+                    # select a session using the tiered distribution
                     selection = getRandomTiered(sessions)
                     # while the selected session in already in the list...
                     while selection in attendeeDict[key] or selection == 0:
@@ -237,7 +257,7 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
     elif distribution == 'skewed':
         # I reference the following url for skewed distribution code
         # https://gamedev.stackexchange.com/questions/116832/random-number-in-a-range-biased-toward-the-low-end-of-the-range
-        print 'Generating skewed distribution...'
+        print 'Sampling from skewed distribution...'
         # loop over the keys in the dict
         for key, value in attendeeDict.iteritems():
             # start with index 0
@@ -246,7 +266,7 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
             for s in attendeeDict[key]:
                 # if there is a default space in the array...
                 if None in attendeeDict[key]:
-                    # select a session
+                    # select a session using the skewed distribution
                     selection = getRandomSkewed(sessions)
                     # while the selected session in already in the list...
                     while selection in attendeeDict[key] or selection == 0:
@@ -267,7 +287,7 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
     elif distribution == 'custom':
         # I referenced the following url for normal distribution code
         # https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.random.normal.html
-        print 'Generating custom distribution...'
+        print 'Sampling from custom distribution...'
         #loop over the keys in the dict
         for key, value in attendeeDict.iteritems():
             # start with index 0
@@ -276,7 +296,7 @@ def generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDic
             for s in attendeeDict[key]:
                 # if there is a default space in the array...
                 if None in attendeeDict[key]:
-                    # select a session
+                    # select a session using the custom distribution
                     selection = getCustom(sessions)
                     # while the selected session in already in the list...
                     while selection in attendeeDict[key] or selection == 0:
@@ -342,11 +362,13 @@ def main():
             maxSessions = 1
 
     # create dictionary of lists to hold the sessions for each attendee
+    # each session spot is initialized to None
     attendeeDict = {}
     for i in range (0, attendees):
         attendeeDict[i + 1] = [None] * maxSessions
 
-    # pass validated inputs to schedule generation function
+    # pass validated inputs to schedule generation function generateSchedule()
+    # to get a dictionary of lists representing each attendee's schedule
     schedule = generateSchedule(sessions, attendees, maxSessions, distribution, attendeeDict)
 
     # enumerate all conflicts
@@ -364,6 +386,7 @@ def main():
     # generate E and P array
     EParrays = generateEPArrays(conflictAdjacency)
 
+    # print the result!
     print { "sessions(N)": sessions, 
     "distinctConflicts(M)": distinctConflicts, 
     "conflicts(T)": conflicts, 
@@ -376,3 +399,77 @@ def main():
 # test if script is being run directly
 if __name__ == '__main__':
     main ()
+
+
+
+def calculateDistinctConflicts():
+    # # define a list of pairwise conflicts
+    # conflicts = [[1,2], [1,2], [2,1], [3,4]]
+
+    # # create a dictionary to hold the distinct conflicts
+    # distinctConflicts = {}
+
+    # # loop over the conflicts list
+    # for pair in conflicts:
+    #     element0 = pair[0]
+    #     element1 = pair[1]
+
+    #     # create a list of keys that currently exist in the distinctConflicts dictionary
+    #     conflictKeys = distinctConflicts.keys()
+
+    #     # if neither element is a key of the distinctConflicts dictionary
+    #     if element0 not in conflictKeys and element1 not in conflictKeys:
+    #         # add key element0 to distinctConflicts and set the value to an empty list
+    #         distinctConflicts[element0] = []
+    #         # assign a var to the new empty list
+    #         targetList = distinctConflicts[element0]
+    #         # append the value of element1 to element0's list
+    #         targetList.append(element1)
+
+    #     # if one of the elements is already a key in distinctConflicts append the value of the other
+    #     # element to the existing keys list
+    #     elif element0 in conflictKeys and element1 not in distinctConflicts[element0]:
+    #         targetList = distinctConflicts[element0]
+    #         targetList.append(element1)
+    #     elif element1 in conflictKeys and element0 not in distinctConflicts[element1]:
+    #         targetList = distinctConflicts[element1]
+    #         targetList.append(element0)
+
+    #     # Otherwise this is a dupicate, do nothing    
+    #     elif element0 in conflictKeys and element1 in distinctConflicts[element0] or element1 in conflictKeys and element0 in distinctConflicts[element1]:
+    #         # duplicate, don't add
+    #         continue
+
+    distinctConflicts = set([frozenset(pair) for pair in conflicts])
+
+    print (distinctConflicts)
+            
+    return distinctConflicts
+
+# def formatConflicts(conflicts):
+#     distinctConflicts = {}
+#     for pair in conflicts:
+#         element0 = pair[0]
+#         element1 = pair[1]
+#         conflictKeys = distinctConflicts.keys()
+#         # if neither element is a key of the distinctConflicts dictionary
+#         if element0 not in conflictKeys and element1 not in conflictKeys:
+#             # add key element0 to distinctConflicts and set the value to an empty list
+#             distinctConflicts[element0] = []
+#             # assign a var to the new empty list
+#             targetList = distinctConflicts[element0]
+#             # append the value of element1 to element0's list
+#             targetList.append(element1)
+#         # if one of the elements is already a key in distinctConflicts append the value of the other
+#         # element to the existing keys list
+#         elif element0 in conflictKeys and element1 not in distinctConflicts[element0]:
+#             targetList = distinctConflicts[element0]
+#             targetList.append(element1)
+#         elif element1 in conflictKeys and element0 not in distinctConflicts[element1]:
+#             targetList = distinctConflicts[element1]
+#             targetList.append(element0)
+#         elif element0 in conflictKeys and element1 in distinctConflicts[element0] or element1 in conflictKeys and element0 in distinctConflicts[element1]:
+#             # duplicate case! 
+#             continue
+
+#     return distinctConflicts
